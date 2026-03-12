b96873dcd96f1a8bca59a90214a7c8e33dfb04d5
ahashmi5401 <ahashmi5401@gmail.com>
2026-03-13 03:12:48 +0500

    do while

---
+++ 04-loops/doWhileLoop.cpp
@@ -0,0 +1,17 @@
+//do While Loop
+//run atleast one time even condition is false 
+
+#include <iostream>
+using namespace std;
+
+main () {
+    int i = 100;
+
+do 
+{
+    cout << i;
+    i++;
+}while (i < 5);
+
+
+}
